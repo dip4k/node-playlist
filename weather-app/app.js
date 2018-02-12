4cfe036550a9334b130dd0850b31e0556dea31e8
dip4k <dip4k.chauhan@outlook.com>
2018-02-12 15:38:01 +0530

    Add forcast.io api code to fetch weather results

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,9 @@
+const request = require("request");
 const yargs = require("yargs");
 
 const geocode = require("./geocode/geocode");
+
+const ApiKey = "52cc995daae16302bef69c78ff3375b9";
 
 const argv = yargs
   .options({
@@ -14,10 +17,26 @@ const argv = yargs
   .help()
   .alias("help", "h").argv;
 
+const getWeatherResult = (weatherApiKey, latitude, longitude) => {
+  request(
+    {
+      url: `https://api.darksky.net/forecast/${weatherApiKey}/${latitude},${longitude}`,
+      json: true
+    },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        console.log(JSON.stringify(body.currently));
+      } else if (response.statusCode === 404) {
+        console.log("Unable to fetch weather....");
+      }
+    }
+  );
+};
+
 geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   if (errorMessage) {
     console.log(errorMessage);
   } else {
-    console.log(JSON.stringify(results));
+    getWeatherResult(ApiKey, results.latitude, results.longitude);
   }
 });
