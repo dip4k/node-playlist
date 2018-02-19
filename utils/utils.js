9df2fa7009e257505b68560e15af783a5b919d37
dip4k <dip4k.chauhan@outlook.com>
2018-02-19 17:38:20 +0530

    finished simple test for synchronous code

--- utils/utils.js
+++ utils/utils.js
@@ -1,2 +1,10 @@
 module.exports.add = (a, b) => a + b;
 module.exports.sqr = a => a ** 2;
+
+module.exports.user = fullName => {
+  const names = fullName.split(" ");
+  const user = {};
+  user.fName = names[0];
+  user.lName = names[1];
+  return user;
+};
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,14 +1,36 @@
+const expect = require("expect");
 const utils = require("./utils");
 
 it("should add two numbers", () => {
   const res = utils.add(10, 23);
-  if (res !== 33) {
-    throw new Error(`Expected 33 but got ${res}.`);
-  }
+
+  // if (res !== 33) {
+  //   throw new Error(`Expected 33 but got ${res}.`);
+
+  expect(res)
+    .toBe(33)
+    .toBeA("number");
 });
 it("should square a number", () => {
   const res = utils.sqr(5);
-  if (res !== 25) {
-    throw new Error(`Expected (sqr of 5) 25 but got ${res}`);
-  }
+  expect(res).toBe(25);
+
+  // if (res !== 25) {
+  //   throw new Error(`Expected (sqr of 5) 25 but got ${res}`);
+  // }
+});
+
+// it("should be some test", () => {
+//   // expect(10).toNotBe(20);
+//   // expect({ name: "dc" }).toNotEqual({ name: "dc" });
+//   // expect({ name: "dc" }).toEqual({ name: "dc" });
+//   // expect({ name: "dc", age: 24, add: "pune" }).toInclude({ age: 24 });
+// });
+
+it("should verify firstName and lastName are set", () => {
+  const res = utils.user("dipak chauhan");
+
+  // expect(res).toEqual({ fName: "dipak", lName: "chauhan" });
+
+  expect(res).toInclude({ fName: "dipak", lName: "chauhan" });
 });
