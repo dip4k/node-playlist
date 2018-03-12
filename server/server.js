22695a4ff08b4137e3c77ce341842d26d0de1b5d
dip4k <dip4k.chauhan@outlook.com>
2018-03-12 15:13:56 +0530

    Add code to test express server

--- server/server.js
+++ server/server.js
@@ -6,10 +6,14 @@ app.get('/', (req, res) => {
   res.status(200).send('hello world');
 });
 
-// app.get('/', (req, res) => {
-//   res.status(200).send({ error: 'page not found' });
-// });
+app.get('/error', (req, res) => {
+  res.status(404).send({ error: 'page not found' });
+});
 
+// returns json
+app.get('/user', (req, res) => {
+  res.status(201).json({ name: 'Dipak', id: 'dip4k' });
+});
 app.listen(3000, () => {
   console.log('server running on port 3000...');
 });
--- server/server.test.js
+++ server/server.test.js
@@ -1,11 +1,81 @@
-const request = require("supertest");
+const supertest = require('supertest');
+// const server = require('./server');
+const app = require('./server').app;
 
-const server = require("./server");
+request = supertest(app);
 
-it("should send hello world response", done => {
-  request(server.app)
-    .get("/")
+/* eslint no-undef:0 */
+it('should send hello world response', done => {
+  request
+    .get('/')
     .expect(200)
-    .expect("hello world")
-    .end(done);
+    .expect('hello world')
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+      done();
+    });
 });
+
+it('should send error response', done => {
+  request
+    .get('/error')
+    .expect(404)
+    .expect({ error: 'page not found' })
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+      done();
+    });
+});
+
+// get json data
+describe('GET /user', () => {
+  it('user.name should be an case-insensitive match for "dipak"', done => {
+    request
+      .get('/user')
+      .set('Accept', 'application/json')
+      .expect(res => {
+        res.body.name = res.body.name.toUpperCase();
+      })
+      .expect(
+        201,
+        {
+          id: 'dip4k',
+          name: 'DIPAK'
+        },
+        done // done can be used here
+      );
+  });
+});
+
+// example with promises
+// describe('GET /user', () => {
+//   it('respond with json', () =>
+//     request
+//       .get('/user')
+//       .set('Accept', 'application/json')
+//       .expect(201)
+//       .then(response => {
+//         assert(response.body.name, 'Dipak');
+//         assert(response.body.id, 'dip4k');
+//       }));
+// });
+
+// Code examples for using supertest with mocha
+// describe('GET /users', () => {
+//   it('respond with json', done => {
+//     request(app)
+//       .get('/users')
+//       .set('Accept', 'application/json')
+//       .expect(200)
+//       .end((err, res) => {
+//         if (err) {
+//           return done(err);
+//         }
+//         done();
+//       });
+//   });
+// });
