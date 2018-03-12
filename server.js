a2fb4c3b434ab81f618d4ebe837e6d9e7129f79b
dip4k <dip4k.chauhan@outlook.com>
2018-03-12 12:37:57 +0530

    Refactor codebase

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const path = require("path");
-const hbs = require("hbs");
-const fs = require("fs");
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
@@ -10,14 +10,14 @@ const port = process.env.PORT || 3000; // for heroku
 // middleware
 
 hbs.registerPartials(`${__dirname}/views/partials`);
-app.set("view engine", "hbs");
+app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
   const timeStamp = new Date().toString();
   const log = `${timeStamp} : ${req.method}  ${req.url}`;
-  fs.appendFile("server.log", `${log}\n`, err => {
+  fs.appendFile('server.log', `${log}\n`, err => {
     if (err) {
-      console.log("unable to write in server.log file.");
+      console.log('unable to write in server.log file.');
     }
   });
   console.log(log);
@@ -32,29 +32,29 @@ app.use(express.static(path.resolve(`${__dirname}/public`)));
 
 // helpers
 
-hbs.registerHelper("getYear", () => new Date().getFullYear());
-hbs.registerHelper("screamIt", text => text.toUpperCase());
+hbs.registerHelper('getYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', text => text.toUpperCase());
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   // console.log("Hello from express...!");
   // res.send({ name: "dipak", likes: ["gaming", "treking"] });
 
-  res.render("home", {
-    title: "Home Page",
-    user: "Dipak ^_^"
+  res.render('home', {
+    title: 'Home Page',
+    user: 'Dipak ^_^'
   });
 });
 
-app.get("/about", (req, res) => {
+app.get('/about', (req, res) => {
   // res.send("This is about page...");
 
-  res.render("about", {
-    title: "About page"
+  res.render('about', {
+    title: 'About page'
   });
 });
 
-app.get("/bad", (req, res) => {
-  res.send({ errorMsg: "404 error occured.." });
+app.get('/bad', (req, res) => {
+  res.send({ errorMsg: '404 error occured..' });
 });
 
 app.listen(port, () => {
