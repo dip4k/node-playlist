103e6d573331cb1f9ae32b807c88e2317d57fe94
dip4k <dip4k.chauhan@outlook.com>
2018-03-19 14:29:22 +0530

    code refactor

--- server/server.js
+++ server/server.js
@@ -23,44 +23,43 @@ app.use(bodyParser.json());
 // create todo
 app.post('/todos', (req, res) => {
   let todo = new Todo({ text: req.body.text });
-  todo.save().then(
-    doc => {
+  todo
+    .save()
+    .then(doc => {
       res.status(201).send(doc);
-    },
-    err => {
+    })
+    .catch(err => {
       res.status(400).send(err);
-    }
-  );
+    });
 });
 
 // get all todos
 app.get('/todos', (req, res) => {
-  Todo.find().then(
-    todos => {
+  Todo.find()
+    .then(todos => {
       res.status(200).send({ todos });
-    },
-    err => {
+    })
+    .catch(err => {
       res.status(400).send(err);
-    }
-  );
+    });
 });
 
 // get todo by id
 app.get('/todos/:id', (req, res) => {
   if (!ObjectID.isValid(req.params.id)) {
-    res.send({ message: 'Invalid ObjectID' });
+    res.status(404);
+    return res.send({ message: 'Invalid ObjectID' });
   }
-  Todo.findById(req.params.id).then(
-    todo => {
+  Todo.findById(req.params.id)
+    .then(todo => {
       if (!todo) {
-        res.send({ message: `no todo item exist with ${req.params.id}` });
+        res.status(404).send({ message: `no todo item exist with ${req.params.id}` });
       }
-      res.status(200).send(todo);
-    },
-    err => {
+      res.status(200).send({ todo });
+    })
+    .catch(err => {
       res.status(400).send(err);
-    }
-  );
+    });
 });
 
 // --- start server
