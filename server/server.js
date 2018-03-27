97292e79b444f6a6d8bba8b6e943534555c80455
dip4k <dip4k.chauhan@outlook.com>
2018-03-27 11:58:24 +0530

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
@@ -9,7 +10,7 @@ const { Todo } = require('./models/todoModel');
 const { User } = require('./models/UserModel');
 
 // Port
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // app
 const app = express();
@@ -25,10 +26,10 @@ app.post('/todos', (req, res) => {
   let todo = new Todo({ text: req.body.text });
   todo
     .save()
-    .then(doc => {
+    .then((doc) => {
       res.status(201).send(doc);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).send(err);
     });
 });
@@ -36,10 +37,10 @@ app.post('/todos', (req, res) => {
 // get all todos
 app.get('/todos', (req, res) => {
   Todo.find()
-    .then(todos => {
+    .then((todos) => {
       res.status(200).send({ todos });
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).send(err);
     });
 });
@@ -51,15 +52,60 @@ app.get('/todos/:id', (req, res) => {
     res.status(404);
     return res.send({ message: 'Invalid ObjectID' });
   }
-  Todo.findById(req.params.id)
-    .then(todo => {
+  Todo.findById(id)
+    .then((todo) => {
       if (!todo) {
-        res.status(404).send({ message: `no todo item exist with $id}` });
+        res.status(404).send({ message: `no todo item exist with id : $id}` });
       }
       res.status(200).send({ todo });
     })
     .catch(() => {
       res.status(400).send();
+    });
+});
+
+// delete todo by id
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send({ err_msg: 'Invalid id' });
+  }
+  Todo.findByIdAndRemove(id)
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send({ message: `no todo item exist with id: ${id}` });
+      }
+      res.status(200).send({ todo });
+    })
+    .catch((e) => {
+      res.status(400).send({ err_msg: e });
+    });
+});
+
+// Update todo
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send({ err_msg: 'Invalid id' });
+  }
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true, runValidators: true })
+    .then((updatedTodo) => {
+      if (!updatedTodo) {
+        return res.status(404).send({ err_msg: 'No todo present with this id' });
+      }
+      res.status(200).send({ updatedTodo });
+    })
+    .catch((e) => {
+      res.status(400).send({ err_msg: e });
     });
 });
 
