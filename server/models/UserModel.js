6397e857ec871a4afd0d715f486875c7f59cc394
dip4k <dip4k.chauhan@outlook.com>
2018-04-03 12:28:39 +0530

    add : jwt sign code on user creation

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,18 +1,65 @@
+const _ = require('lodash');
 const mongoose = require('mongoose');
 const Schema = require('mongoose').Schema;
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+
+const keys = require('../../config/keys');
 
 let userSchema = new Schema(
   {
     email: {
       required: true,
       type: String,
+      minlength: 4,
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: '{VALUE} is not a valid email id!'
+      }
+    },
+    password: {
+      type: String,
+      required: true,
       minlength: 4
-    }
+    },
+    tokens: [
+      {
+        access: {
+          type: String,
+          required: true
+        },
+        token: {
+          type: String,
+          required: true
+        }
+      }
+    ]
   },
   {
     collection: 'Users'
   }
 );
+/* eslint func-names:0  */
+/* eslint no-underscore-dangle:0 */
+
+userSchema.methods.generateAuthTokens = function() {
+  let user = this;
+  let access = 'auth';
+  let token = jwt
+    .sign({ _id: user._id.toHexString(), access }, keys.authSecret)
+    .toString();
+  user.tokens.push({ access, token });
+  console.log(user);
+  return user.save().then(() => token);
+};
+
+userSchema.methods.toJSON = function() {
+  let user = this;
+  let userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
 
 let User = mongoose.model('User', userSchema);
 
--- server/server.js
+++ server/server.js
@@ -4,13 +4,15 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+
 // import mongoose conn
 // const { mongoose } = require('./db/mongoose');
 require('./db/mongoose');
-// model should always come after mongoose connection
-const { Todo } = require('./models/todoModel');
-const { User } = require('./models/UserModel');
 
+// model should always come after mongoose connection
+const User = require('./models/UserModel').User;
+
+const todosRoutes = require('./routes/todosRoute');
 // Port
 const port = process.env.PORT;
 
@@ -22,92 +24,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // --- handling requests
+// todos route
+app.use('/todos', todosRoutes);
+// User routes --->>
+app.post('/users', (req, res, next) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  let user = new User(body);
 
-// create todo
-app.post('/todos', (req, res) => {
-  let todo = new Todo({ text: req.body.text });
-  todo
-    .save()
-    .then((doc) => {
-      res.status(201).send(doc);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
-});
-
-// get all todos
-app.get('/todos', (req, res) => {
-  Todo.find()
-    .then((todos) => {
-      res.status(200).send({ todos });
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
-});
-
-// get todo by id
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id;
-  if (!ObjectID.isValid(id)) {
-    res.status(404);
-    return res.send({ message: 'Invalid ObjectID' });
-  }
-  Todo.findById(id)
-    .then((todo) => {
-      if (!todo) {
-        res.status(404).send({ message: `no todo item exist with id : $id}` });
-      }
-      res.status(200).send({ todo });
-    })
-    .catch(() => {
-      res.status(400).send();
-    });
-});
-
-// delete todo by id
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id;
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send({ err_msg: 'Invalid id' });
-  }
-  Todo.findByIdAndRemove(id)
-    .then((todo) => {
-      if (!todo) {
-        return res.status(404).send({ message: `no todo item exist with id: ${id}` });
-      }
-      res.status(200).send({ todo });
+  user
+    .generateAuthTokens()
+    .then((token) => {
+      res.header('x-auth', token).send(user.toJSON());
     })
     .catch((e) => {
-      res.status(400).send({ err_msg: e });
-    });
-});
-
-// Update todo
-app.patch('/todos/:id', (req, res) => {
-  let id = req.params.id;
-  let body = _.pick(req.body, ['text', 'completed']);
-
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send({ err_msg: 'Invalid id' });
-  }
-  if (_.isBoolean(body.completed) && body.completed) {
-    body.completedAt = new Date().getTime();
-  } else {
-    body.completed = false;
-    body.completedAt = null;
-  }
-
-  Todo.findByIdAndUpdate(id, { $set: body }, { new: true, runValidators: true })
-    .then((updatedTodo) => {
-      if (!updatedTodo) {
-        return res.status(404).send({ err_msg: 'No todo present with this id' });
-      }
-      res.status(200).send({ todo: updatedTodo });
-    })
-    .catch((e) => {
-      res.status(400).send({ err_msg: e });
+      res.status(400).send(e);
     });
 });
 
