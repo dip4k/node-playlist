924c83a38b9af9ee11de7459614afb5860c230a4
dip4k <dip4k.chauhan@outlook.com>
2018-04-05 17:46:43 +0530

    add: bcryptjs code to hashing playground

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,37 @@
 const { SHA256 } = require('crypto-js');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-let data = { id: 20 };
+const bcrypt = require('bcryptjs');
+
+// using bcrypt
+// hash password
+bcrypt
+  .genSalt(5)
+  .then((salt) => {
+    bcrypt.hash('user password to be hashed', salt).then((hashPassword) => {
+      console.log(JSON.stringify({ hashPassword }, undefined, 2));
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+let hash = '$2a$10$4XjvuHllI01qijyEyX7RZuLDH.7MHPcPBsfq/2rRwRoxgKrSYrExC';
+// compare hashPassword withoriginal
+bcrypt.compare('user password to be ', hash).then((isValid) => {
+  console.log(JSON.stringify({ isValid }, undefined, 2));
+});
+
+// let data = { id: 20 };
 
 // let token = jwt.sign({ data, exp: 60 * 60 }, 'secret key or salt');
-let token = jwt.sign(data, 'secret key or salt', { expiresIn: '1h' });
+// let token = jwt.sign(data, 'secret key or salt', { expiresIn: '1h' });
 // let token = jwt.sign(data, 'secret key or salt');
 
-console.log(token);
+// console.log(token);
 
-let decoded = jwt.verify(token, 'secret key or salt');
-console.log(decoded);
+// let decoded = jwt.verify(token, 'secret key or salt');
+// console.log(decoded);
 // const CryptoJS = require('crypto-js');
 
 // console.log(CryptoJS.MD5('dipak', 'abcd').toString());
